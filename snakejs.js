575189bb8eaa65143049829cf2c9b3fcd8572a81
hussainsarwari <mohammadhussainsarwari5@gmail.com>
2022-10-28 15:19:59 +0430

    accident added

--- snakejs.js
+++ snakejs.js
@@ -36,10 +36,12 @@ if(e.keyCode==37){//left
 stop1=setInterval(()=>{
    g-=1;
    snake.style.left=g+"px";
-   },0.6)
+   },7 )
    clearInterval(stop2 );
    clearInterval(stop3);
    clearInterval(stop4);
+   direction="right_left"
+   set_direction();
 }
 else if(e.keyCode==38)
 {
@@ -47,27 +49,33 @@ else if(e.keyCode==38)
   stop2= setInterval(()=>{//top
       h-=1;
       snake.style.top=h+"px";
-      },0.6)
+      },7)
       clearInterval(stop1);
       clearInterval(stop3);
       clearInterval(stop4);
+      direction="top_down";
+      set_direction();
 }
 if(e.keyCode==39){
   stop3= setInterval(()=>{//right
       g+=1;
       snake.style.left=g+"px";
-      },0.6)
+      },7)
       clearInterval(stop1);
       clearInterval(stop2);
       clearInterval(stop4);
+      direction="right_left"
+      set_direction();
 }else if(e.keyCode==40){//down
 stop4=setInterval(() => {
    h+=1;
    snake.style.top=h+"px";
-}, 0.6);    
+}, 7);    
 clearInterval(stop1);
 clearInterval(stop2);
 clearInterval(stop3);
+direction="top_down";
+set_direction();
 }
 }   
 
@@ -77,15 +85,34 @@ clearInterval(stop3);
 
 seed_possion();
 
-
+increase_snake();
 
 
 
 
 // function
+let newElement;
+let d=20;
+function increase_snake(){
+   setInterval(()=>{
+      if((parseInt(snake.style.top)==parseInt(seed.style.top)-20) &&( (parseInt(snake.style.left)>=parseInt(seed.style.left)-20) &&    parseInt(snake.style.left)<=parseInt(seed.style.left)+20) //accident to top
+      ){
+ 
 
-function increase_snake(){}
+      }else  if((parseInt(snake.style.left)>=parseInt(seed.style.left)-20)&&(parseInt(snake.style.left)<=(parseInt(seed.style.left)+20) &&( (parseInt(snake.style.top)>=parseInt(seed.style.top)-20) && (   parseInt(snake.style.top)<=parseInt(seed.style.top)+20)) //accident to top
+      )){
+   
+      }
 
+   },0.3)
+}
+function set_direction(){
+if (direction=="right_left"){
+   snake.style.transform="rotate(0deg)";
+}else if(direction=="top_down"){
+   snake.style.transform="rotate(90deg)";
+}
+}
 
 function seed_possion(){
   seed.style.left=ran1+"px";
